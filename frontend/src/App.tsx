f8539d674fccee3ba13c9feaae9e78c929e1e502
Victory Ma <victoryma23@gmail.com>
2026-02-02 16:42:42 -0600

    Added tailwind styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,15 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div className="flex h-screen flex-col items-center justify-center">
+        <button className="bg-emerald-700 text-white border-2 border-gray-800 p-4 rounded-lg hover:bg-emerald-400 hover:cursor-pointer hover:text-gray-80 hover:transition duration-200 ease-in-out" onClick={() => setCount((count) => count + 1)}>
           Click me... {count}
         </button>
-        <p>
+        <p className="mt-4"> 
           Hello World!
         </p>
       </div>
